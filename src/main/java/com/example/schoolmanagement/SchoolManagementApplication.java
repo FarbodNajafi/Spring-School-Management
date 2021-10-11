5984275c9f7d30212b2aadc8fd868df7661b7cfb
Farbod Najafi <farbodnajafi@gmail.com>
2021-10-11 10:22:10 +0330

    Enabled Spring Security

--- src/main/java/com/example/schoolmanagement/SchoolManagementApplication.java
+++ src/main/java/com/example/schoolmanagement/SchoolManagementApplication.java
@@ -2,11 +2,9 @@ package com.example.schoolmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-}
-)
+@SpringBootApplication
 public class SchoolManagementApplication {
 
     public static void main(String[] args) {
