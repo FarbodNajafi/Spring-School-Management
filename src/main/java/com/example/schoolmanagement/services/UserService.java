5d27e5aaf92ffb28781d36731eae7f56eead5abd
Farbod Najafi <farbodnajafi@gmail.com>
2021-10-16 11:19:37 +0330

    Replaced fake user repository with the actual user repository

--- src/main/java/com/example/schoolmanagement/services/UserService.java
+++ src/main/java/com/example/schoolmanagement/services/UserService.java
@@ -1,28 +1,35 @@
 package com.example.schoolmanagement.services;
 
+import com.example.schoolmanagement.entities.CustomUserDetails;
+import com.example.schoolmanagement.entities.User;
 import com.example.schoolmanagement.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Autowired
-    public UserService(@Qualifier("fake") UserRepository userRepository) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.getUserByUsername(username)
-                .orElseThrow(
-                        () -> new UsernameNotFoundException("Username " + username + " not found")
-                );
+//        return userRepository.getUserByUsername(username)
+//                .orElseThrow(
+//                        () -> new UsernameNotFoundException("Username " + username + " not found")
+//                );
+        Optional<User> user = userRepository.findByUsername(username);
+        user.orElseThrow(() -> new UsernameNotFoundException("Username not found: " + username));
+        return user.map(CustomUserDetails::new).get();
     }
 
     public UserDetails getUserByUsername(String username) throws UsernameNotFoundException {
